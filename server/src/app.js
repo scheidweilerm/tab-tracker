d469c896c073a1c455e66c607c9f9fc419f8c19f
mike <mike@londres.uk>
2018-05-08 13:02:38 +0100

    Second commit server install

---
+++ server/src/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
